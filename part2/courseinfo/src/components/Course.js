0fbd55d4e40076a8296531aed1a40fd3c45a0208
Francis <francisbernal14@gmail.com>
2023-06-25 15:09:59 +0200

    feat: this is exercise 2.2 and 2.3

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './Header';
 import Content from './Content';
+import Total from './Total';
 
 const Course = ({ course }) => {
     const name = course.name;
     const content = course.parts
+    const totalExercises = content.reduce((s, p) => s + p.exercises, 0)
 
     return (
         <div>
             <Header text={name} />
             <Content content={content} />
+            <Total value={totalExercises} />
         </div>
     )
 }
---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,3 @@
+const Total = ({value}) => <p>Total of {value} exercises</p>
+
+export default Total;
