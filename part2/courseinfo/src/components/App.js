b760ca2fa76deb17a7bbe3927b455a746910a35f
Francis <francisbernal14@gmail.com>
2023-06-25 15:28:33 +0200

    feat: this is exercise 2.4 and 2.5

--- part2/courseinfo/src/components/App.js
+++ part2/courseinfo/src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 import Course from './Course'
 
 const App = () => {
-    const course = {
-      id: 1,
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -21,10 +22,40 @@ const App = () => {
           exercises: 14,
           id: 3,
         },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4,
+        },
       ],
-    }
-  
-    return <Course course={course} />
-  }
-  
-  export default App
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ]
+
+  return (
+    <>
+      {
+        courses.map(course => {
+          return <Course key={course.id} course={course} />
+        })
+      }
+    </>
+  )
+}
+
+export default App
