82000d9c79486f44b63974e98c69ee3f339f71c2
Francis <francisbernal14@gmail.com>
2023-07-11 22:16:01 +0200

    feat: this is exercise 4.2

---
+++ part4/bloglist/app.js
@@ -0,0 +1,21 @@
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const express = require('express')
+const app = express()
+const blogRouter = require('./controllers/blogs')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+mongoose
+    .connect(config.MONGO_URI)
+    .then(() => {
+        logger.info("Connected to MongoDB successfully!")
+    })
+    .catch(error => logger.error(error.message))
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
