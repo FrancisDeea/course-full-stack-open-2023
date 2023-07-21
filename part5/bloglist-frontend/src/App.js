8e37ae46844f51ba54a5369ab9241f63e61b3223
Francis <francisbernal14@gmail.com>
2023-07-21 05:48:16 +0200

    feat: this is exercise 5.1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,23 +1,79 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+  console.log(blogs)
 
-  return (
+  const loginForm = () => (
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <label>
+          Username:{" "}
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+        {" "}
+        <label>
+          Passsword:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+        {" "}
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  )
+
+  const renderBlogs = () => (
     <div>
       <h2>blogs</h2>
+      <p>{user.username} is logged in!</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      console.log(exception.message)
+    }
+  }
+
+  useEffect(() => {
+    blogService.getAll()
+      .then(initialBlogs => setBlogs(initialBlogs))
+  }, [])
+
+  return (
+    <>
+      {
+        user === null ?
+          loginForm() :
+          renderBlogs()
+      }
+    </>
   )
 }
 
