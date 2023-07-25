aeb353261039639e2177141b8492b0ccd9ee8a38
Francis <francisbernal14@gmail.com>
2023-07-26 01:07:27 +0200

    feat: this is exercise 5.9

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -5,9 +5,11 @@ const Blogs = ({ blogs, handleLikes }) => {
     return (
         <div>
             <h2>blogs</h2>
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} handleLikes={handleLikes} />
-            )}
+            {blogs
+                .sort((a, b) => b.likes - a.likes)
+                .map(blog =>
+                    <Blog key={blog.id} blog={blog} handleLikes={handleLikes} />
+                )}
         </div>
     )
 }
