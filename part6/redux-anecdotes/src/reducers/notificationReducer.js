02d9bfb192fdc2bee39973f67534e6b7bfe55751
Francis <francisbernal14@gmail.com>
2023-08-03 21:49:22 +0200

    feat: this is exercise 6.12

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = "render here notification..."
+
+const notificationSlice = createSlice({
+    name: "notification",
+    initialState,
+    reducers: {
+
+    }
+})
+
+export default notificationSlice.reducer
